f071d15ead0bb7ff639996c867c346c6367375a3
Baggers <chris.bagley@gmail.com>
2017-05-15 16:41:09 +0200

    Let's start using the MediaQuery api

--- Examples/Basics/MainView.js
+++ Examples/Basics/MainView.js
@@ -1,15 +1,28 @@
 var Observable = require("FuseJS/Observable");
+var MediaQuery = require("FuseJS/MediaQuery");
 var StreamingPlayer = require("FuseJS/StreamingPlayer");
 
 var doonIt = false;
 
 var foo = function() {
-    StreamingPlayer.play({"id":0,
-                         "name":"fooName",
-                         "artist":"MrFoo",
-                         "url":"https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3",
-                         "artworkUrl":"https://everyweeks.com/pZ2j56doGB6c0ykra8lXMj7nuNTDsT79-logo.png",
-                         "duration":10});
+	var query = {
+		"artist": "Alan Gogoll"
+	};
+	MediaQuery.tracks(query).then(function(results) {
+		var tracks = results.map(function(track, index) {
+			return {
+				"id": index,
+				"name": "jam jam jam", //track["name"],
+				"artist": track["artist"],
+				"url": track["path"],
+				"artworkUrl":"https://everyweeks.com/pZ2j56doGB6c0ykra8lXMj7nuNTDsT79-logo.png",
+				"duration": track["duration"]
+			};
+		});
+		StreamingPlayer.setPlaylist(tracks);
+	}).catch(function(e) {
+		console.log("Well damn:" + e);
+	});
 };
 
 module.exports = {
